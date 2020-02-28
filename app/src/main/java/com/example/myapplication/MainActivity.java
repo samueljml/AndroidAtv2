d22df13edf4bfc03b38bdc59190a8fa8fa09cdeb
Gabriel Andrade <certamentenaoeobiel@gmail.com>
2020-02-28 13:21:37 -0300

    Versão final, pronta para ser entregue.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    Toast.makeText(this, "Versão final", Toast.LENGTH_SHORT).show();
 }
+
